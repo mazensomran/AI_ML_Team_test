f8e022d9e94bfd2c2881378f7379dc1d2a794cf7
mazensomran <mazenomran2020@gmail.com>
2024-11-14 16:22:16 +0300

    first version

--- draw_plots.py
+++ draw_plots.py
@@ -16,88 +16,107 @@ class PlotDrawer:
         plot_paths = []
 
         #График 1: Распределение истинных значений "gt_corners"
+        plt.figure()
         sns.histplot(self.data['gt_corners'], kde=True, color='green')
         plt.xlabel('Grand Truth corners')
         plt.title('Distribution of Grand Truth corners')
         plot_path = os.path.join(self.plots_dir, 'gt_corners_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         # График 2: Распределение прогнозируемых значений "rb_corners"
+        plt.figure()
         sns.histplot(self.data['rb_corners'], kde=True, color='green')
         plt.xlabel('Predicted corners')
         plt.title('Distribution of Predicted corners')
         plot_path = os.path.join(self.plots_dir, 'rb_corners_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         # График 3: gt_corners и rb_corners
+        plt.figure()
         plt.scatter(self.data['gt_corners'], self.data['rb_corners'], color='blue')
         plt.xlabel('Ground Truth Corners')
         plt.ylabel('Model Predicted Corners')
         plt.title('Ground Truth vs Predicted Corners')
         plot_path = os.path.join(self.plots_dir, 'gt_vs_predicted.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         #График 4: Распределение средних отклонений "mean"
+        plt.figure()
         sns.histplot(self.data['mean'], kde=True, color='green')
         plt.xlabel('Mean Deviation')
         plt.title('Distribution of Mean Deviation')
         plot_path = os.path.join(self.plots_dir, 'mean_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         #График 5: Распределение максимальных отклонений "max"
+        plt.figure()
         sns.histplot(self.data['max'], kde=True, color='red')
         plt.xlabel('Max Deviation')
         plt.title('Distribution of Max Deviation')
         plot_path = os.path.join(self.plots_dir, 'max_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         # График 6: Распределение минимальных отклонений "max"
+        plt.figure()
         sns.histplot(self.data['min'], kde=True, color='blue')
         plt.xlabel('Min Deviation')
         plt.title('Distribution of Min Deviation')
         plot_path = os.path.join(self.plots_dir, 'min_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         #График 7: Максимальные отклонения против минимальных отклонений
+        plt.figure()
         plt.scatter(self.data['min'], self.data['max'], s=32, alpha=.8)
         plt.xlabel('Min vs Max Distribution')
         plt.title('Distribution of Min-Max Deviation')
         plot_path = os.path.join(self.plots_dir, 'min_max_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         #График 8: Средние отклонения против максимальных отклонений
+        plt.figure()
         plt.scatter(self.data['mean'], self.data['max'], s=32, alpha=.8)
         plt.xlabel('Mean vs Max Distribution')
         plt.title('Distribution of Mean-Max Deviation')
         plot_path = os.path.join(self.plots_dir, 'mean_max_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         #График 9: Средние отклонения против минимальных отклонений
+        plt.figure()
         plt.scatter(self.data['mean'], self.data['min'], s=32, alpha=.8)
         plt.xlabel('Mean vs Min Distribution')
         plt.title('Distribution of Mean-Min Deviation')
         plot_path = os.path.join(self.plots_dir, 'mean_min_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         #График 10: Распределение средних значений
+        plt.figure()
         plt.hist(self.data['mean'], bins=20, alpha=0.5, label='Overall Mean')
         plt.hist(self.data['floor_mean'], bins=20, alpha=0.5, label='Floor Mean')
         plt.hist(self.data['ceiling_mean'], bins=20, alpha=0.5, label='Ceiling Mean')
@@ -107,10 +126,12 @@ class PlotDrawer:
         _ = plt.legend()
         plot_path = os.path.join(self.plots_dir, 'mean_Values_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         # График 11: Распределение максимальных значений "max"
+        plt.figure()
         plt.hist(self.data['max'], bins=20, alpha=0.5, label='Overall Max')
         plt.hist(self.data['floor_max'], bins=20, alpha=0.5, label='Floor Max')
         plt.hist(self.data['ceiling_max'], bins=20, alpha=0.5, label='Ceiling Max')
@@ -120,10 +141,12 @@ class PlotDrawer:
         _ = plt.legend()
         plot_path = os.path.join(self.plots_dir, 'max_Values_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
         #График 12: Распределение минимальных значений
+        plt.figure()
         plt.hist(self.data['min'], bins=20, alpha=0.5, label='Overall Min')
         plt.hist(self.data['floor_min'], bins=20, alpha=0.5, label='Floor Min')
         plt.hist(self.data['ceiling_min'], bins=20, alpha=0.5, label='Ceiling Min')
@@ -133,6 +156,7 @@ class PlotDrawer:
         _ = plt.legend()
         plot_path = os.path.join(self.plots_dir, 'min_Values_distribution.png')
         plt.savefig(plot_path)
+        plt.close()
         plot_paths.append(plot_path)
         #plt.show()
 
